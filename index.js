89602ddfe9265ec4a12df4d8b9f3358199f6f1c5
ammangame748-dev <ammangame748@gmail.com>
2026-05-06 11:21:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const port = process.env.PORT || 3000; // مهم جداً عشان Render
 const fs = require('fs');
 
 const app = express();
